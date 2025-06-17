f1b99351620841c916fe426203f6cc820d420370
Parth Sakpal <101584071+parthsakpal109@users.noreply.github.com>
2025-06-17 16:44:40 +0530

    Delete CRUD Operations directory

--- CRUD Operations/com.hexaware.crudassign1/src/main/java/com/example/demo/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, String> {
-}
